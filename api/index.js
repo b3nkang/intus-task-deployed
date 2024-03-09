b5d7655c54d125b8dcdc1deeff4c9c27a6a4daed
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-03-09 00:56:16 -0500

    change for vercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-const http = require("http");
 const { app } = require("./app");
 
-const PORT = 5002;
-
-function startServer() {
-  const server = http.createServer(app);
-  server.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
-}
-
-startServer();
+module.exports = app;
